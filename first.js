a298e0636a578bab106e69ec2b5eafa83334509e
MagomedFR <magomed.khaptukae@gmai.com>
2023-07-04 14:03:56 +0300

    gg

---
+++ first.js
@@ -0,0 +1,4 @@
+import {a,b,f} from './index.js'
+import c from './index.js'
+console.log(f(a,b))
+console.log(c)
---
+++ index.js
@@ -0,0 +1,8 @@
+const a = 2;
+const b = 3;
+const c = 4;
+function f(a,b){
+    return a+b
+}
+export {a,b,f}
+export default c
